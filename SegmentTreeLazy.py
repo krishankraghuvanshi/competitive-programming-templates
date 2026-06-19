82c20d2fc7ecf080e6174002c8b2b32ff020397f
Krishank Raghuvanshi <148806405+krishankraghuvanshi@users.noreply.github.com>
2026-06-19 15:00:55 +0530

    Add SegmentTree class with lazy propagation methods

--- SegmentTreeLazy.py
+++ SegmentTreeLazy.py
@@ -66,3 +66,54 @@ class SegmentTree:
 		return res
 	def __repr__(self):
 		return "LazySegmentTree({0})".format(self.data)
+```---------------------------------------------------```
+class SegmentTree:
+    def __init__(self, N):
+        self.N = N
+        self.tree = [0] * (4*N)
+        self.lazy = [0] * (4*N)
+
+    def push(self, index, left, right):
+        if self.lazy[index] == 0:
+            return
+        self.tree[index] += (right-left+1) * self.lazy[index]
+        if left != right:
+            self.lazy[2*index+1] += self.lazy[index]
+            self.lazy[2*index+2] += self.lazy[index]
+        self.lazy[index] = 0     
+
+    def update(self, index, left, right, pos, value):
+        self.push(index, left, right)
+        if left == right:
+            self.tree[index] += value
+            return 
+        mid = (left + right) // 2
+        if pos <= mid:
+            self.update(2*index+1, left, mid, pos, value)
+        else:
+            self.update(2*index+2, mid+1, right, pos, value)
+        self.tree[index] = self.tree[2*index+1]+self.tree[2*index+2]
+        
+    def range_update(self, index, left, right, q_left, q_right, value):
+        self.push(index, left, right)
+        if left > q_right or right < q_left:
+            return 
+        if q_left <= left and right <= q_right:
+            self.lazy[index]+=value
+            self.push(index, left, right)
+            return 
+        mid = (left + right) // 2
+        self.range_update(2*index+1, left, mid, q_left, q_right, value)
+        self.range_update(2*index+2, mid+1, right, q_left, q_right, value)
+        self.tree[index] = self.tree[2*index+1]+self.tree[2*index+2]
+
+    def query(self, index, left, right, q_left, q_right):
+        self.push(index, left, right)
+        if left > q_right or right < q_left:
+            return 0
+        if q_left <= left and right <= q_right:
+            return self.tree[index]
+        mid = (left + right) // 2
+        return (self.query(2*index+1, left, mid, q_left, q_right) +
+                 self.query(2*index+2, mid+1, right, q_left, q_right))
+        
