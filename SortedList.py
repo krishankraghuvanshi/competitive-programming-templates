11a94094cc9858bf7988aa8a02cbbdde19be85f4
Krishank Raghuvanshi <148806405+krishankraghuvanshi@users.noreply.github.com>
2026-09-18 04:02:24 +0530

    Update SortedList.py

--- SortedList.py
+++ SortedList.py
@@ -1,123 +1,49 @@
-from typing import Generic, Iterable, Iterator, List, Tuple, TypeVar, Optional
-T = TypeVar('T')
+
+from bisect import bisect_left, bisect_right, insort
+from typing import Generic, TypeVar, Iterator
+
+T = TypeVar("T")
+
+
 class SortedList(Generic[T]):
-    BUCKET_RATIO = 16
-    SPLIT_RATIO = 24
-    def __init__(self, a: Iterable[T] = []) -> None:
-        a = list(a)
-        n = self.size = len(a)
-        if any(a[i] > a[i + 1] for i in range(n - 1)):
-            a.sort()
-        num_bucket = int(math.ceil(math.sqrt(n / self.BUCKET_RATIO)))
-        self.a = [a[n * i // num_bucket : n * (i + 1) // num_bucket] for i in range(num_bucket)]
-    def __iter__(self) -> Iterator[T]:
-        for i in self.a:
-            for j in i: yield j
-    def __reversed__(self) -> Iterator[T]:
-        for i in reversed(self.a):
-            for j in reversed(i): yield j
-    def __eq__(self, other) -> bool:
-        return list(self) == list(other)
+    def __init__(self):
+        self.data: list[T] = []
+
+    def add(self, x: T) -> None:
+        insort(self.data, x)
+
+    def remove(self, x: T) -> None:
+        i = bisect_left(self.data, x)
+
+        if i == len(self.data) or self.data[i] != x:
+            raise ValueError(f"{x} not in SortedList")
+
+        self.data.pop(i)
+
+    def discard(self, x: T) -> None:
+        i = bisect_left(self.data, x)
+
+        if i < len(self.data) and self.data[i] == x:
+            self.data.pop(i)
+
+    def bisect_left(self, x: T) -> int:
+        return bisect_left(self.data, x)
+
+    def bisect_right(self, x: T) -> int:
+        return bisect_right(self.data, x)
+
+    def __getitem__(self, index: int) -> T:
+        return self.data[index]
+
     def __len__(self) -> int:
-        return self.size
-    def __repr__(self) -> str:
-        return "SortedMultiset" + str(self.a)
-    def __str__(self) -> str:
-        s = str(list(self))
-        return "{" + s[1 : len(s) - 1] + "}"
-    def _position(self, x: T) -> Tuple[List[T], int, int]:
-        for i, a in enumerate(self.a):
-            if x <= a[-1]: break
-        return (a, i, bisect_left(a, x))
+        return len(self.data)
+
     def __contains__(self, x: T) -> bool:
-        if self.size == 0: return False
-        a, _, i = self._position(x)
-        return i != len(a) and a[i] == x
-    def count(self, x: T) -> int:
-        return self.index_right(x) - self.index(x)
-    def insert(self, x: T) -> None:
-        if self.size == 0:
-            self.a = [[x]]
-            self.size = 1
-            return
-        a, b, i = self._position(x)
-        a.insert(i, x)
-        self.size += 1
-        if len(a) > len(self.a) * self.SPLIT_RATIO:
-            mid = len(a) >> 1
-            self.a[b:b+1] = [a[:mid], a[mid:]]
-    def _pop(self, a: List[T], b: int, i: int) -> T:
-        ans = a.pop(i)
-        self.size -= 1
-        if not a: del self.a[b]
-        return ans
-    def remove(self, x: T) -> bool:
-        if self.size == 0: return False
-        a, b, i = self._position(x)
-        if i == len(a) or a[i] != x: return False
-        self._pop(a, b, i)
-        return True
-    def lt(self, x: T) -> Optional[T]:
-        for a in reversed(self.a):
-            if a[0] < x:
-                return a[bisect_left(a, x) - 1]
-    def le(self, x: T) -> Optional[T]:
-        for a in reversed(self.a):
-            if a[0] <= x:
-                return a[bisect_right(a, x) - 1]
-    def gt(self, x: T) -> Optional[T]:
-        for a in self.a:
-            if a[-1] > x:
-                return a[bisect_right(a, x)]
-    def ge(self, x: T) -> Optional[T]:
-        for a in self.a:
-            if a[-1] >= x:
-                return a[bisect_left(a, x)]
-    def __getitem__(self, i: int) -> T:
-        if i < 0:
-            for a in reversed(self.a):
-                i += len(a)
-                if i >= 0: return a[i]
-        else:
-            for a in self.a:
-                if i < len(a): return a[i]
-                i -= len(a)
-        raise IndexError
-    def pop(self, i: int = -1) -> T:
-        if i < 0:
-            for b, a in enumerate(reversed(self.a)):
-                i += len(a)
-                if i >= 0: return self._pop(a, ~b, i)
-        else:
-            for b, a in enumerate(self.a):
-                if i < len(a): return self._pop(a, b, i)
-                i -= len(a)
-        raise IndexError
-    def index(self, x: T) -> int:
-        ans = 0
-        for a in self.a:
-            if a[-1] >= x:
-                return ans + bisect_left(a, x)
-            ans += len(a)
-        return ans
-    def index_right(self, x: T) -> int:
-        ans = 0
-        for a in self.a:
-            if a[-1] > x:
-                return ans + bisect_right(a, x)
-            ans += len(a)
-        return ans
-    def find_closest(self, k: T) -> Optional[T]:
-        if self.size == 0:
-            return None
-        ltk = self.le(k)
-        gtk = self.ge(k)
-        if ltk is None:
-            return gtk
-        if gtk is None:
-            return ltk
-        if abs(k-ltk)<=abs(k-gtk):
-            return ltk
-        else:
-            return gtk
-        
+        i = bisect_left(self.data, x)
+        return i < len(self.data) and self.data[i] == x
+
+    def __iter__(self) -> Iterator[T]:
+        return iter(self.data)
+
+    def __repr__(self) -> str:
+        return repr(self.data)
